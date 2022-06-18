e1563f6228bb72571006edb8903cdf5cf84718f6
Roman Dodin <dodin.roman@gmail.com>
2022-06-18 11:51:59 +0200

    make linter happy

--- boxen/platforms/ciscocsr1000v.go
+++ boxen/platforms/ciscocsr1000v.go
@@ -141,7 +141,7 @@ func (p *CiscoCsr1000v) Install(opts ...instance.Option) error {
 	c := make(chan error, 1)
 	stop := make(chan bool, 1)
 
-	go func() { //nolint:dupl
+	go func() {
 		err = p.Qemu.Start(opts...)
 		if err != nil {
 			c <- err
